9cddb8df3c70eb1fb4d86a7a56455adf5b8eeec4
Patrick Cartlidge <patrickcartlidge@gmail.com>
2025-09-01 15:33:33 +0100

    Add cookies, cookie banner and analytics
    This uses mostly the same implementation as the GOV.UK Design System
    website (alphagov#govuk-design-system). The most significant change is
    that there are no `campaign` cookies (as there are currently no embeds
    on the site that use external cookies).

---
+++ _includes/javascripts/analytics.mjs
@@ -0,0 +1,23 @@
+// from https://github.com/alphagov/govuk-design-system/blob/main/src/javascripts/components/analytics.mjs
+
+export function loadAnalytics() {  
+  if (!window.ga || !window.ga.loaded) {
+    // Load gtm script
+    // Script based on snippet at https://developers.google.com/tag-manager/quickstart
+    // prettier-ignore
+    ;(function (w, d, s, l, i) {
+      w[l] = w[l] || []
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      })
+
+      const j = d.createElement(s)
+      const dl = l !== 'dataLayer' ? `&l=${l}` : ''
+
+      j.async = true
+      j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`
+      document.head.appendChild(j)
+    })(window, document, 'script', 'dataLayer', 'GTM-NDQ4DC87')
+  }
+}
---
+++ _includes/javascripts/application.mjs
@@ -0,0 +1,5 @@
+import { loadAnalytics } from './analytics.mjs';
+import { getConsentCookie, isValidConsentCookie } from './cookie-functions.mjs'
+import CookieBanner from './cookie-banner.mjs';
+import CookiesPage from './cookies-page.mjs';
+import { createAll } from 'govuk-frontend';
